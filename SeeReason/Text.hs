1e6e57b87eba7b6fe46df05187dd4233c0373deb
Clifford Beshers <Clifford Beshers clifford.beshers@gmail.com>
2021-05-07 11:06:17 -0700

    initial import

---
+++ SeeReason/Text.hs
@@ -0,0 +1,21 @@
+module SeeReason.Text
+    ( diffText
+    ) where
+
+import Data.Algorithm.DiffContext (getContextDiff, prettyContextDiff)
+--import Data.Char (isUpper, toUpper)
+--import Data.ListLike (groupBy)
+--import Data.String (IsString)
+import Data.Text (split, Text, unpack)
+--import qualified Data.Text.Lazy as Lazy ( fromStrict, pack, Text, toStrict, unpack )
+import qualified Text.PrettyPrint as HPJ
+
+-- | Output the difference between two string in the style of diff(1).  This
+-- can be used with Test.HUnit.assertString:  assertString (diffText ("a", "1\n2\n3\n"), ("b", "1\n3\n"))
+diffText :: (String, Text) -> (String, Text) -> String
+diffText (nameA, textA) (nameB, textB) =
+    show (prettyContextDiff
+          (HPJ.text nameA)
+          (HPJ.text nameB)
+          (HPJ.text . unpack)
+          (getContextDiff 2 (split (== '\n') textA) (split (== '\n') textB)))
