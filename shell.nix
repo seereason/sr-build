0d4bf3c7da87f4926c0a72f4d125a3807778524b
Jeremy Shaw <jeremy@n-heptane.com>
2021-05-19 11:41:35 -0400

    use shelly 1.9

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ let
        haskell.packages.ghc865.developPackage
         { root = ./.;
           name = cabalName;
+          overrides = self: super: { shelly = super.shelly_1_9; };
           modifier = drv: haskell.lib.addBuildDepends drv
             (with haskell.packages.ghc865;
               # add extra ghc libraries here
